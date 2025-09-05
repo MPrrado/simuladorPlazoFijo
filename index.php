c2f4900027c28c41dd3e5e79eb55ef059987235e
Prado Matias Santiago <matias14prado@gmail.com>
2025-09-05 14:15:16 -0300

    se modifico el index.php

--- index.php
+++ index.php
@@ -2,9 +2,9 @@
     require_once('php/encabezado.php');
 
 ?>
-<section>
+<section class="">
     <article>
-        <form action="control.php">
+        <form action="php/control.php" method="post">
             <legend>DATOS A SIMULAR</legend>
             <section>
                 <label for="tna">TNA</label>
@@ -13,25 +13,39 @@
             <section>
                 <label for="plazo">PLAZO</label>
                 <select name="plazo" id="plazo">
+                    <option value="">------SELECCIONE------</option>
                     <option value="1">1 mes</option>
-                    <option value="2">2 mes</option>
-                    <option value="3">3 mes</option>
-                    <option value="4">4 mes</option>
-                    <option value="5">5 mes</option>
-                    <option value="6">6 mes</option>
-                    <option value="7">7 mes</option>
-                    <option value="8">8 mes</option>
-                    <option value="9">9 mes</option>
-                    <option value="10">10 mes</option>
-                    <option value="11">11 mes</option>
-                    <option value="12">12 mes</option>
+                    <option value="2">2 meses</option>
+                    <option value="3">3 meses</option>
+                    <option value="4">4 meses</option>
+                    <option value="5">5 meses</option>
+                    <option value="6">6 meses</option>
+                    <option value="7">7 meses</option>
+                    <option value="8">8 meses</option>
+                    <option value="9">9 meses</option>
+                    <option value="10">10 meses</option>
+                    <option value="11">11 meses</option>
+                    <option value="12">12 meses</option>
                 </select>
             </section>
-            <section>
-                <label for=""></label>
-                <input type="text">
+            <section class="" id="contenedor-interes-compuesto">
+                <p>INTERES COMPUESTO</p>
+                <section>
+                    <label for="si">SI</label>
+                    <input type="radio" name="interesCompuesto" id="si" value="si">
+                </section>
+                <section>
+                    <label for="no">NO</label>
+                    <input type="radio" id="no" name="interesCompuesto" value="no">
+                </section>
+                <section>
+                    <label for="monto">MONTO: </label>
+                    <input type="number" id="monto" name="monto">
+                </section>
             </section>
-
+            <section>
+                <input type="submit" value='SIMULAR'>
+            </section>
 
         </form>
     </article>
