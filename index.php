3e3dacadf7f43122a0c0a9fd1681cc663b28cc3c
Prado Matias Santiago <matias14prado@gmail.com>
2025-09-11 12:31:48 -0300

    se avanzo en la implementacion del interes compuesto y con el script

--- index.php
+++ index.php
@@ -24,6 +24,25 @@
                     <label for="no">NO</label>
                     <input type="radio" id="no" name="interesCompuesto" value="no">
                 </section>
+                <section id="cotenedor-meses">
+                    <label for="meses">CANTIDAD DE MESES:</label>
+                    <select name="meses" id="">
+                        <option value="">--------SELECCIONE--------</option>
+                        <option value="1">1</option>
+                        <option value="2">2</option>
+                        <option value="3">3</option>
+                        <option value="4">4</option>
+                        <option value="5">5</option>
+                        <option value="6">6</option>
+                        <option value="7">7</option>
+                        <option value="8">8</option>
+                        <option value="9">9</option>
+                        <option value="10">10</option>
+                        <option value="11">11</option>
+                        <option value="12">12</option>
+                    </select>
+
+                </section>
                 <section>
                     <label for="monto">MONTO: </label>
                     <input type="number" id="monto" name="monto" requierd placeholder="$1000000">
---
+++ js/script.js
@@ -0,0 +1,10 @@
+let meses = document.getElementById('cotenedor-meses');
+let interesCompuesto = document.getElementById('si');
+
+function activarSeleccionadorMeses()
+{
+    if(interesCompuesto.checked)
+    {
+        meses.style.display = blocks
+    }
+}
